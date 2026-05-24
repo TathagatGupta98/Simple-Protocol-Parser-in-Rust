668b38d29d2182a8f73eb5bb1d562b76304148f4
TathagatGupta98 <janaktathagat@gmail.com>
2026-05-24 10:54:32 +0530

    feat: Completed the protocol parser with proper error handling

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,141 @@ pub enum Command{
 }
 
 #[derive(Eq,PartialEq,Debug)]
-pub enum error{
+pub enum Error{
+    UnexpectedNewline,
+    IncompleteMessage,
+    EmptyMessage,
+    UnknownCommand,
+    UnexpectedPayload,
+    MissingPayload,
+}
 
+fn parse(input: &str)-> Result<Command, Error>{
+
+    let Some(req) = input.strip_suffix('\n') else{
+        return Err(Error::IncompleteMessage)
+    };
+
+    if true == req.contains('\n'){
+        return Err(Error::UnexpectedNewline)
+    }
+
+    if req == "" {
+        return Err(Error::EmptyMessage)
+    }
+
+    if let Some(url) = req.strip_prefix("PUBLISH ") {
+        Ok(Command::Publish(String::from(url)))
+    } else if req == "PUBLISH"{
+        Err(Error::MissingPayload)
+    } else if req == "RETRIEVE" {
+        Ok(Command::Retrieve)
+    } else if let Some(url) = req.strip_prefix("RETRIEVE ") {
+        Err(Error::UnexpectedPayload)
+    } else if req == "" {
+        Err(Error::EmptyMessage)
+    } else {
+        Err(Error::UnknownCommand)
+    }
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Tests placement of \n
+    #[test]
+    fn test_missing_nl() {
+        let line = "RETRIEVE";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Err(Error::IncompleteMessage);
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn test_trailing_data() {
+        let line = "PUBLISH The message\n is wrong \n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Err(Error::UnexpectedNewline);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_empty_string() {
+        let line = "";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Err(Error::IncompleteMessage);
+        assert_eq!(result, expected);
+    }
+
+    // Tests for empty messages and unknown commands
+
+    #[test]
+    fn test_only_nl() {
+        let line = "\n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Err(Error::EmptyMessage);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_unknown_command() {
+        let line = "SERVE \n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Err(Error::UnknownCommand);
+        assert_eq!(result, expected);
+    }
+
+    // Tests correct formatting of RETRIEVE command
+
+    #[test]
+    fn test_retrieve_w_whitespace() {
+        let line = "RETRIEVE \n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Err(Error::UnexpectedPayload);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_retrieve_payload() {
+        let line = "RETRIEVE this has a payload\n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Err(Error::UnexpectedPayload);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_retrieve() {
+        let line = "RETRIEVE\n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Ok(Command::Retrieve);
+        assert_eq!(result, expected);
+    }
+
+    // Tests correct formatting of PUBLISH command
+
+    #[test]
+    fn test_publish() {
+        let line = "PUBLISH TestMessage\n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Ok(Command::Publish("TestMessage".into()));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_empty_publish() {
+        let line = "PUBLISH \n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Ok(Command::Publish("".into()));
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_missing_payload() {
+        let line = "PUBLISH\n";
+        let result: Result<Command, Error> = parse(line);
+        let expected = Err(Error::MissingPayload);
+        assert_eq!(result, expected);
+    }
 }
